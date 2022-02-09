36d38c896dc96ffe3b1ea43497a4cf8dce099211
ZHAO Xudong <zxdong@gmail.com>
2022-02-09 08:09:40 +0800

    Fix serverless

--- src/server/lambda.js
+++ src/server/lambda.js
@@ -6,5 +6,5 @@ import serverlessHTTP from 'serverless-http'
 import app1 from './app/app'
 
 export const app = serverlessHTTP(app1, {
-  binary: ['application/octet-stream']
+  binary: ['application/octet-stream', 'application/gzip']
 })
